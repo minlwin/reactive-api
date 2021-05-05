af66558cc38b9978fa653f6be45d1a3c335328f8
minlwin <lwin.zawmin@gmail.com>
2021-05-05 23:58:25 +0630

    Update JSON Auto Detact Mode

--- f01-hello/src/main/java/com/jdc/flux/hello/model/Todo.java
+++ f01-hello/src/main/java/com/jdc/flux/hello/model/Todo.java
@@ -1,10 +1,11 @@
 package com.jdc.flux.hello.model;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import org.springframework.data.annotation.Id;
 
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record Todo(
         @Id Integer id,
         String assign,
         String description,
-        boolean completed) {
-}
+        Boolean complete) { }
