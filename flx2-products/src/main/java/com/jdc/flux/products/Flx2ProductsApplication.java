c8fa5a12398cf30f60a6bb97dc232995e018af4e
minlwin <lwin.zawmin@gmail.com>
2021-05-06 10:22:55 +0630

    Create Spring Project by IDE

---
+++ flx2-products/src/main/java/com/jdc/flux/products/Flx2ProductsApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.flux.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Flx2ProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Flx2ProductsApplication.class, args);
+	}
+
+}
---
+++ flx2-products/src/test/java/com/jdc/flux/products/Flx2ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.flux.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Flx2ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
